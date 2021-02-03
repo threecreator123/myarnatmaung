70f6833d6caf079514fce1f65eb6e0fff5638334
ANT <we.threecreator@gmail.com>
2021-02-03 17:52:25 +0630

    project files

---
+++ src/main/java/mm/myarnatmaung/MyarnatmaungApplication.java
@@ -0,0 +1,13 @@
+package mm.myarnatmaung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyarnatmaungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyarnatmaungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mm/myarnatmaung/MyarnatmaungApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.myarnatmaung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyarnatmaungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
